8b3240ec90d3c8413c3bd1467746b7a7851a775b
Seu Nome <seuemail@exemplo.com>
2026-07-15 17:16:29 -0300

    fix: force dynamic AS clinic landing route

--- app/pg-asclinica/page.tsx
+++ app/pg-asclinica/page.tsx
@@ -1,5 +1,7 @@
 import ClinicPage from "@/app/(public)/clinica/[slug]/page";
 
+export const dynamic = "force-dynamic";
+
 export default function AsClinicaPage() {
   return <ClinicPage params={Promise.resolve({ slug: "clinica-bela" })} />;
 }
